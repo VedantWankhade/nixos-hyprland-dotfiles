9206e0f51ce1f0dfbabfd7e1b9ae162715104f03
VedantWankhade <vedantwankhade17@gmail.com>
2024-03-11 22:38:30 +0530

    Vim options

--- .config/nvim/lua/custom/settings.lua
+++ .config/nvim/lua/custom/settings.lua
@@ -1,1 +1,15 @@
 vim.cmd('colorscheme catppuccin-mocha')
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "120"
