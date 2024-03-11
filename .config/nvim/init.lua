c7e8006a01cb02d697f155b9ef83c63e62ba10ad
VedantWankhade <vedantwankhade17@gmail.com>
2024-03-11 19:32:15 +0530

    Nvim - some remaps and catppuccin theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,2 @@
-require("custom")
 require("lazypm")
-
+require("custom")
--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,2 +1,8 @@
+local builtin = require('telescope.builtin')
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })	
+end
+)
