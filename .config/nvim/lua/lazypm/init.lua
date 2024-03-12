6c8e4197b7c016b2356327addec6fefa0d3e555c
VedantWankhade <vedantwankhade17@gmail.com>
2024-03-12 15:11:46 +0530

    Neovim - minor statusline change

--- .config/nvim/lua/lazypm/init.lua
+++ .config/nvim/lua/lazypm/init.lua
@@ -29,6 +29,6 @@ require('lualine').setup {
     options = {
         theme = "catppuccin",
         section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' }
+        component_separators = { left = ')', right = '(' }
     }
 }
