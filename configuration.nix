1663f7bd6784d73647ce6c3e55f741c817f76585
VedantWankhade <vedantwankhade17@gmail.com>
2024-03-15 17:04:49 +0530

    fzf theme

--- configuration.nix
+++ configuration.nix
@@ -171,6 +171,11 @@
     fi
 
     # export GTK_THEME='Catppuccin-Mocha-Compact-Mauve-Dark'
+    # fzf theme
+    export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
   '';
  
   # Editor
