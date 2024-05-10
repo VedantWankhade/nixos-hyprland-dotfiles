cc26d9030ea3cc27d0899225f89f2189739ef2e9
VedantWankhade <vedantwankhade17@gmail.com>
2024-05-11 00:00:40 +0530

    overwrite which key to fix lsp keymaps

--- .config/nvim/lua/lazypm/lsp.lua
+++ .config/nvim/lua/lazypm/lsp.lua
@@ -3,7 +3,10 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false,
+    })
 end)
 
 require('mason').setup({})
