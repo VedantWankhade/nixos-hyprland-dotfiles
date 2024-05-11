5be239a489197523fb006634a644af57bbdeb95c
VedantWankhade <vedantwankhade17@gmail.com>
2024-05-11 15:12:55 +0530

    <C-p> for git files

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.g.mapleader = " "
+vim.keymap.set('n', '<C-p>', builtin.git_files, {desc='Search git files'})
 vim.keymap.set('n', '<leader>f', builtin.find_files, {desc='Search files'})
-vim.keymap.set('n', '<leader>fp', builtin.git_files, {desc= 'Search project files'})
+vim.keymap.set('n', '<leader>fp', builtin.git_files, {desc= 'Search git files'})
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>g', ':LazyGit<CR>')
 vim.keymap.set('n', '<leader>y', '"+y', {desc='Yank (copy) to system clipboard'})
