fc8bc6cc4218eba57f1c974674b71ea4b79eff0c
VedantWankhade <vedantwankhade17@gmail.com>
2024-05-11 12:16:09 +0530

    remaps for system clipboard

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,6 +1,15 @@
 local builtin = require('telescope.builtin')
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>f', builtin.find_files, {desc='Search files'})
-vim.keymap.set('n', '<leader>p', builtin.git_files, {desc= 'Search project files'})
+vim.keymap.set('n', '<leader>fp', builtin.git_files, {desc= 'Search project files'})
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>g', ':LazyGit<CR>')
+vim.keymap.set('n', '<leader>y', '"+y', {desc='Past from system clipboard at cursor'})
+vim.keymap.set('n', '<leader>p', '"+p', {desc='Past from system clipboard at cursor'})
+vim.keymap.set('n', '<leader>P', '"+P', {desc='Past from system clipboard above cursor'})
+vim.keymap.set('v', '<leader>y', '"+y', {desc='Past from system clipboard at cursor'})
+vim.keymap.set('v', '<leader>p', '"+p', {desc='Past from system clipboard at cursor'})
+vim.keymap.set('v', '<leader>P', '"+P', {desc='Past from system clipboard above cursor'})
+
+
+
