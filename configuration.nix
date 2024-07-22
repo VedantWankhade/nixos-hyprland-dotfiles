e2f06300e22eef37bf1aa0ced0792209fd6b1e53
VedantWankhade <vedantwankhade17@gmail.com>
2024-07-22 22:31:44 +0530

    nixpkgs: add gnumake

--- configuration.nix
+++ configuration.nix
@@ -265,8 +265,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    gnumake
     lxqt.lxqt-policykit
     tree
     wl-gammarelay-rs
