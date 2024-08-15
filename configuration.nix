4c58ff323065bb7427fdbaa76924a82f439a1097
VedantWankhade <vedantwankhade17@gmail.com>
2024-08-15 12:28:33 +0530

    config: nvidia gpu driver and install steam

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,56 @@
   powerManagement.enable = true;
   services.tlp.enable = true;
 
+  # Epic Gaming (its just steam)
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+    extraPackages = [ pkgs.gamescope ];
+  };
+
+  # GPU
+  hardware.opengl = {
+    enable = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    # powerManagement.enable = false;
+  
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # nvidia optimus
+    prime = {
+    sync.enable = true;
+      # Make sure to use the correct Bus ID values for your system!
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
   # Virtualisation
   virtualisation = {
     containers.enable = true;
@@ -102,26 +152,24 @@
   services.gnome.gnome-keyring.enable = true;
 
   # snmp
-  # only enable to make this device an snmp agent
-  # to install snmp manager just install net-snmp 
-  #services.snmpd = {
-  #  enable = true;
-  #  openFirewall = true;
-  #  configText = ''
-  #    # Listen for connections from the local machine only
-  #    agentAddress  tcp:127.0.0.1:161
-  #
-  #    # Allow SNMPv1/v2c read access with a community string
-  #    rocommunity public localhost
-  #
-  #    # System information
-  #    sysLocation  "MH, IND"
-  #    sysContact   "vedantwankhade17@gmail.com"
-  #
-  #    # Trap settings (optional)
-  #    trapsink 127.0.0.1:162 public
-  #  '';
-  #};
+  services.snmpd = {
+    enable = true;
+    openFirewall = true;
+    configText = ''
+      # Listen for connections from the local machine only
+      agentAddress  tcp:127.0.0.1:161
+
+      # Allow SNMPv1/v2c read access with a community string
+      rocommunity public localhost
+
+      # System information
+      sysLocation  "MH, IND"
+      sysContact   "vedantwankhade17@gmail.com"
+
+      # Trap settings (optional)
+      trapsink 127.0.0.1:162 public
+    '';
+  };
 
   # SSH
   services.openssh.enable = true;
@@ -267,6 +315,7 @@
     extraGroups = [ "networkmanager" "wheel" "docker" "podman" ];
     packages = with pkgs; [
       discord
+      aseprite
       podman-desktop
       kdePackages.kate
       slack
