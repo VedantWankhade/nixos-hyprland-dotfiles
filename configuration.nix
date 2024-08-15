f8a5cfbb633867c8cabec198a8f3e01ea0248bf7
VedantWankhade <vedantwankhade17@gmail.com>
2024-08-16 01:50:16 +0530

    nixpkgs: wine

--- configuration.nix
+++ configuration.nix
@@ -379,6 +379,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wineWowPackages.stable
     virt-manager
     virt-viewer
     spice spice-gtk
